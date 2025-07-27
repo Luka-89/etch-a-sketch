ccaf4707fc5dd0829fca8913d111a8d44be512b7
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-27 20:55:10 +0200

    Make resizing work

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ let sizeInput = document.querySelector("#size");
 let size = sizeInput.value;
 let sketchZone = document.querySelector("#sketch-zone");
 let resizeButton = document.querySelector("#size-btn");
+resizeButton.addEventListener("click", render);
 
 let mouseDown = false;
 document.addEventListener("mousedown", () => mouseDown = true);
 document.addEventListener("mouseup", () => mouseDown = false);
 
-for(let i = 0; i < size; i++) {
-    let gridRow = document.createElement("div");
-    for(let j = 0; j < size; j++) {
-        let tile = document.createElement("div");
-        tile.classList.add("tile");
-        gridRow.appendChild(tile);
-        tile.addEventListener("mouseover", changeColor);
-    }
-    sketchZone.appendChild(gridRow);
-}
+render();
 
 function changeColor(e) {
-    if(!mouseDown) return;
+    if (!mouseDown) return;
     e.target.style.backgroundColor = "var(--jet-light)";
+}
+
+function render() {
+    sketchZone.innerHTML = "";
+    let sketchZoneSize = 700;
+    let tileSize = Math.floor(sketchZoneSize / parseInt(sizeInput.value));
+
+    sketchZoneSize = tileSize * parseInt(sizeInput.value);
+    sketchZone.style.width = sketchZoneSize + "px";
+    sketchZone.style.height = sketchZoneSize + "px";
+
+    for (let i = 0; i < parseInt(sizeInput.value); i++) {
+        let gridRow = document.createElement("div");
+        for (let j = 0; j < parseInt(sizeInput.value); j++) {
+            let tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.style.height = tileSize + "px";
+            tile.style.width = tileSize + "px";
+            gridRow.appendChild(tile);
+            tile.addEventListener("mouseover", changeColor);
+        }
+        sketchZone.appendChild(gridRow);
+    }
 }
